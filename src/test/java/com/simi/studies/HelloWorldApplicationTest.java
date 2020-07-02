b95f4775138208ceee24fc3cace9c70757a012aa
Felipe Miranda <felip.simi@gmail.com>
2020-07-02 05:04:24 -0300

    Setup do JUnit: inclusao de dependencia e classe teste basica

--- src/test/java/com/simi/studies/HelloWorldApplicationTest.java
+++ src/test/java/com/simi/studies/HelloWorldApplicationTest.java
@@ -1,8 +1,13 @@
 package com.simi.studies;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class HelloWorldApplicationTest {
 
+  @Test
   public void applicationStartup() {
+    Assertions.assertDoesNotThrow(() -> HelloWorldApplication.main(null));
   }
 
 }
