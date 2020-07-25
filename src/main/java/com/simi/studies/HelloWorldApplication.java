1e6aa9b27fa5abfce302a68b52d2318d010676af
Felipe Miranda <felip.simi@gmail.com>
2020-07-25 18:54:55 -0300

    AspectJ: anotacao na classe main @ExceptionTest

--- src/main/java/com/simi/studies/HelloWorldApplication.java
+++ src/main/java/com/simi/studies/HelloWorldApplication.java
@@ -1,7 +1,10 @@
 package com.simi.studies;
 
+import com.simi.studies.aspectj.ExceptionTest;
+
 public class HelloWorldApplication {
 
+  @ExceptionTest
   public static void main(String[] args) {
     System.out.println("Bem vindo ao curso de Apache Maven");
   }
